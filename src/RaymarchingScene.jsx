37064a06c43ca88fff6df86f19e2f813be7f1814
Christian Hohenbild <mail@christianhohenbild.de>
2024-07-11 22:56:29 +0200

    add local screen space uvs

--- src/RaymarchingScene.jsx
+++ src/RaymarchingScene.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useRef, useMemo } from "react"
 import { useFrame, useThree } from "@react-three/fiber"
 import * as THREE from "three"
 
@@ -9,15 +9,19 @@ export default function RaymarchingScene() {
   const materialRef = useRef()
   const { camera } = useThree()
   const cameraDirection = useRef(new THREE.Vector3())
+  const inverseModelMatrix = useMemo(() => new THREE.Matrix4(), [])
 
   useFrame((state) => {
     if (materialRef.current && meshRef.current) {
+      meshRef.current.updateMatrixWorld()
+
       materialRef.current.uniforms.uTime.value = state.clock.elapsedTime
       materialRef.current.uniforms.uResolution.value.set(
         state.size.width,
         state.size.height
       )
       materialRef.current.uniforms.uCameraPosition.value.copy(camera.position)
+
       // Calculate camera direction
       camera.getWorldDirection(cameraDirection.current)
       materialRef.current.uniforms.uCameraDirection.value.copy(
@@ -32,12 +36,27 @@ export default function RaymarchingScene() {
       materialRef.current.uniforms.uViewProjectionMatrix.value.copy(
         viewProjectionMatrix
       )
+      // Calculate the inverse
+      inverseModelMatrix.copy(meshRef.current.matrixWorld).invert()
 
-      // Calculate the inverse model matrix
-      // const inverseModelMatrix = meshRef.current.matrixWorld.inverse()
-      // materialRef.current.uniforms.uInverseModelMatrix.value.copy(
-      //   inverseModelMatrix
-      // )
+      // Update the uniform
+      materialRef.current.uniforms.uInverseModelMatrix.value.copy(
+        inverseModelMatrix
+      )
+      const worldPosition = meshRef.current.getWorldPosition(
+        new THREE.Vector3()
+      )
+      const viewPosition = worldPosition
+        .clone()
+        .applyMatrix4(camera.matrixWorldInverse)
+
+      // Project to screen space
+      const screenPosition = worldPosition.project(camera)
+
+      // Update uniforms
+      materialRef.current.uniforms.uCubePosition.value.copy(screenPosition)
+      materialRef.current.uniforms.uCubeViewPosition.value.copy(viewPosition)
+      materialRef.current.uniforms.uCubeScale.value.copy(meshRef.current.scale)
     }
   })
 
@@ -45,7 +64,7 @@ export default function RaymarchingScene() {
     <mesh
       ref={meshRef}
       rotation={[Math.PI / 4, Math.PI / 8, Math.PI / 2]}
-      position={[0.1, 0, 0]}
+      position={[0, 0, 0]}
     >
       <boxGeometry args={[3, 3, 3]} />
       <shaderMaterial
@@ -59,6 +78,9 @@ export default function RaymarchingScene() {
           uCameraDirection: { value: new THREE.Vector3() },
           uViewProjectionMatrix: { value: new THREE.Matrix4() },
           uInverseModelMatrix: { value: new THREE.Matrix4() },
+          uCubePosition: { value: new THREE.Vector3() },
+          uCubeViewPosition: { value: new THREE.Vector3() },
+          uCubeScale: { value: new THREE.Vector3() },
         }}
       />
     </mesh>
--- src/shaders/orthographic.js
+++ src/shaders/orthographic.js
@@ -27,8 +27,12 @@ export const fragmentShader = `
 uniform vec3 uCameraPosition;
 uniform vec3 uCameraDirection;
 uniform mat4 uViewProjectionMatrix;
+uniform mat4 uInverseModelMatrix;
 uniform vec2 uResolution;
 uniform float uTime;
+uniform vec3 uCubePosition;
+uniform vec3 uCubeViewPosition;
+uniform vec3 uCubeScale;
 
 varying mat4 vInverseModelMatrix;
 varying mat4 vModelMatrix;
@@ -53,18 +57,50 @@ float sceneSDF(vec3 p) {
 void main() {
 
     
-    vec2 uv = (gl_FragCoord.xy - 0.5 * uResolution.xy) / uResolution.y;
+    // vec2 uv = (gl_FragCoord.xy - 0.5 * uuResolution.xy) / uuResolution.y;
+
+        // Get screen space coordinates
+    vec2 screenCoords = gl_FragCoord.xy;
+
+    // Calculate aspect ratio
+    float aspectRatio = uResolution.x / uResolution.y;
+
+    // Convert screen space coordinates to UVs in the range [0, 1]
+    vec2 screenUVs = screenCoords / (uResolution);
+    
+    // Convert to [-1, 1] range
+    vec2 mappedNdc = screenUVs * 2.0 - 1.0;
+    
+    // Calculate local screen space coordinates by substracting the cube's position
+    vec2 localScreenSpace = (mappedNdc - uCubePosition.xy);
+
+    // applying the aspect ration
+    localScreenSpace.x *= aspectRatio;
+
+    // center the UVs
+    localScreenSpace = localScreenSpace * 2.0 - 1.0;
+   
+    // scale UVs from center
+    // localScreenSpace = ((localScreenSpace - vec2(0.5)) *  -cubeViewPosition.z / 5. / cubeScale.x) + vec2(0.5);
+    localScreenSpace = ((localScreenSpace / 2. + 0.5) *  -uCubeViewPosition.z / 12. / uCubeScale.x) + vec2(0.5);
+    
+    localScreenSpace = localScreenSpace * 2.0 - 1.0;
+
+    vec2 uv = localScreenSpace;
+
     vec3 forward = uCameraDirection;
     vec3 right = normalize(cross(vec3(0.0, 1.0, 0.0), forward));
     vec3 up = cross(forward, right);
 
     // // Compute the ray origin based on the orthographic projection
     vec3 ro = uCameraPosition + uv.x * right + uv.y * up;
-    ro = (vInverseModelMatrix * vec4(ro, 1.0)).xyz;
+    // ro = (vInverseModelMatrix * vec4(ro, 1.0)).xyz;
+    ro = (uInverseModelMatrix * vec4(ro, 1.0)).xyz;
 
     // // The ray direction is constant and in Object Space
     vec3 rd = forward;
-    rd = (vInverseModelMatrix * vec4(rd, 1.0)).xyz;
+    // rd = (vInverseModelMatrix * vec4(rd, 1.0)).xyz;
+    rd = (uInverseModelMatrix * vec4(rd, 1.0)).xyz;
     rd = normalize(rd);
 
     // Raymarching
@@ -87,5 +123,6 @@ void main() {
     vec3 color = normal * 0.5 + 0.5;
 
     gl_FragColor = vec4(color, 1.0);
+    // gl_FragColor = vec4(uv, 0.0, 1.0);
 }
 `
